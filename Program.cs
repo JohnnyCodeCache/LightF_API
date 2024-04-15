5b378969f437f3a9f068d3fbfbd6e09f0b4599f6
JohnnyCodeCache <brian@zizboo.com>
2024-04-15 03:15:37 -0500

    fixed CORS issue

--- Program.cs
+++ Program.cs
@@ -4,12 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<GetDataFromAPI>();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+      policy => policy
+      .WithOrigins("*")
+      .AllowAnyHeader()
+      .AllowAnyMethod()
+      //.WithOrigins("https://localhost:7115")
+));
 
 var app = builder.Build();
 
@@ -21,6 +29,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors();
 
 app.UseAuthorization();
 
